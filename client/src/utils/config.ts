6816302f928b314778e3d499f686cf6cec97c021
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-05-22 14:14:00 +0100

    edit

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   aws: {
-    region: process.env.NEXT_PUBLIC_AWS_REGION!,
+    region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
     cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || 'us-east-1_6k3vuDzu9',
+      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || 'g6j6sb09igcm2gfd0dg5eu98u'
     }
   },
   api: {
-    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://0djagpee6l.execute-api.us-east-1.amazonaws.com/prod',
   }
 };
